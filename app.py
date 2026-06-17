3d80d18635d7f6f7b9cdd04b6c44e6608fb72fde
lananh26pham-spec <lananh26pham@gmail.com>
2026-06-17 21:02:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Cấu hình trang web của ứng dụng
 st.set_page_config(page_title="App Tính Thuế TNCN Việt Nam_ĐỀ TÀI 7_ PHẠM THỊ LAN ANH", page_icon="💰", layout="centered")
-
+st.logo("logo.jpg")
 st.title("💰 Ứng Dụng Tính Thuế Thu Nhập Cá Nhân")
 st.subheader("ĐỀ TÀI 7_PHẠM THỊ LAN ANH")
 st.write("Cập nhật theo quy định pháp luật thuế hiện hành tại Việt Nam")
